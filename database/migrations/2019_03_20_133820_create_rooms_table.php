ed206b077570bfbbd3da3a41296c5976c51b2f7d
Frederick Roegiers <frederick@rogerthat.be>
2019-03-20 14:46:30 +0100

    [WIP]

---
+++ database/migrations/2019_03_20_133820_create_rooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('number');
+
+            $table->float('price', 8, 2)->default(50);	
+            $table->integer('capacity')->default(2);
+            $table->integer('amountOfBeds')->default(1)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
---
+++ database/migrations/2019_03_20_134326_create_reservations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->date('date_checkin');
+            $table->date('date_checkout');
+
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('clients');
+
+            $table->integer('reservation_id')->unsigned();
+            $table->foreign('reservation_id')->references('id')->on('reservations');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
