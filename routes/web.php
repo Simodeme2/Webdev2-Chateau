c023f5ff47f27ef7c677a6d4283491e59bb95790
Frederick Roegiers <frederick@rogerthat.be>
2019-03-20 13:01:39 +0100

    dynamic routes added

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
+
+/*
+
+// dynamic routes 
+Route::name('pages.')->group(function () {
+    Route::get('/', 'HomeController@getIndex')->name('home');
+});
+
+Route::name('clients.')->group(['middleware' => ['auth']], function () {
+    Route::get('/klanten', 'ClientController@getIndex')->name('index');
+    Route::get('/klanten/nieuw', 'ClientController@getCreate')->name('new');
+    Route::get('/klanten/bewerk/{clientId}', 'ClientController@getEdit')->name('edit');
+    Route::post('/klanten/bewaar', 'ClientController@postSave')->name('save');
+});
+
+Route::name('reservations.')->group(function () {
+    Route::get('/reservaties', 'ReservationsController@getIndex')->name('index');
+    Route::get('/reservaties/nieuw/{clientId}', 'ReservationsController@getCreate')->name('new');
+    Route::get('/reservaties/bewerk/{reservationId}', 'ReservationsController@getEdit')->name('edit');
+    Route::post('/reservaties/bewaar', 'ReservationsController@postSave')->name('save');
+});
+*/
+
+
+/*
+// static routes
 Route::get('/', function () {return view('pages.home');})->name('home');
 
 Route::get('/klanten', function () {return view('clients.index');})->name('clients');
@@ -21,3 +47,4 @@ Route::get('/klanten/bewerk/{client_id}', function ($client_id) {return view('cl
 Route::get('/reservaties', function () {return view('reservations.index');})->name('reservations');
 Route::get('/reservaties/nieuw/{client_id}', function ($client_id) {return view('reservations.edit', ['client_id' => $client_id]);})->name('reservations.create');
 Route::get('/reservaties/bewerk/{reservation_id}', function ($reservation_id) {return view('reservations.edit', ['id' => $reservation_id]);})->name('reservations.edit');
+*/
